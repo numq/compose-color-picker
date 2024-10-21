8b13ec55f2db7a4da254a8b6be412ba973a80653
numq <numq.dev@gmail.com>
2024-10-21 04:34:15 +0300

    Provide refactoring

--- src/main/kotlin/library/picker/core/ColorPickerComponent.kt
+++ src/main/kotlin/library/picker/core/ColorPickerComponent.kt
@@ -1,36 +1,30 @@
 package library.picker.core
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.geometry.isSpecified
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PixelMap
-import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.toPixelMap
 import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
-import library.colorForPosition
-import library.createBitmap
+import library.offset.OffsetPercentage
 
 @Composable
 fun ColorPickerComponent(
     modifier: Modifier,
     indicatorThickness: Float,
     indicatorRadius: Float,
-    onColorChange: ((Color) -> Unit)? = null,
     mapIndicatorOffset: (PointerInputScope.(Offset) -> Offset?)? = null,
-    indicatorContent: DrawScope.(Offset) -> Unit = { indicatorOffset ->
+    indicatorOffsetPercentage: OffsetPercentage,
+    onIndicatorOffsetPercentage: (indicatorOffsetPercentage: OffsetPercentage) -> Unit,
+    indicatorContent: DrawScope.(indicatorOffset: Offset) -> Unit = { indicatorOffset ->
         drawCircle(
             color = Color.Black,
             radius = indicatorRadius / 2f,
@@ -46,85 +40,53 @@ fun ColorPickerComponent(
     },
     content: DrawScope.() -> Unit,
 ) {
-    val changeColor by rememberUpdatedState(onColorChange)
+    val updatedMapIndicatorOffset by rememberUpdatedState(mapIndicatorOffset)
 
-    val mapOffset by rememberUpdatedState(mapIndicatorOffset)
+    val updatedOnIndicatorOffsetPercentage by rememberUpdatedState(onIndicatorOffsetPercentage)
 
-    val drawScope = remember { CanvasDrawScope() }
-
-    var indicatorOffsetPercent by remember { mutableStateOf(Offset.Unspecified) }
-
-    val (pixelMap, setPixelMap) = remember { mutableStateOf<PixelMap?>(null) }
-
-    BoxWithConstraints(modifier = modifier, contentAlignment = Alignment.Center) {
-        val paletteSize = remember(maxWidth, maxHeight) {
-            Size(maxWidth.value, maxHeight.value)
+    BoxWithConstraints(modifier = modifier) {
+        val colorPickerSize = remember(maxWidth, maxHeight) {
+            Size(width = maxWidth.value, height = maxHeight.value)
         }
 
-        val indicatorOffset by remember(paletteSize, indicatorOffsetPercent) {
+        val indicatorOffset by remember(indicatorOffsetPercentage, colorPickerSize) {
             derivedStateOf {
-                if (indicatorOffsetPercent.isSpecified) {
-                    Offset(
-                        x = (indicatorOffsetPercent.x * paletteSize.width).coerceIn(0f, paletteSize.width),
-                        y = (indicatorOffsetPercent.y * paletteSize.height).coerceIn(0f, paletteSize.height)
-                    )
-                } else Offset.Unspecified
+                Offset(
+                    x = (indicatorOffsetPercentage.x * colorPickerSize.width).coerceIn(0f, colorPickerSize.width),
+                    y = (indicatorOffsetPercentage.y * colorPickerSize.height).coerceIn(0f, colorPickerSize.height)
+                )
             }
         }
 
-        val color by remember(pixelMap, indicatorOffset) {
-            derivedStateOf {
-                if (indicatorOffset.isSpecified) {
-                    pixelMap?.let { pixelMap ->
-                        colorForPosition(pixelMap = pixelMap, position = Offset(indicatorOffset.x, indicatorOffset.y))
-                    } ?: Color.Unspecified
-                } else Color.Unspecified
-            }
-        }
-
-        LaunchedEffect(color) {
-            changeColor?.invoke(color)
-        }
-
-        val bitmap by remember(paletteSize, content, color) {
-            derivedStateOf {
-                paletteSize.takeIf(Size::isSpecified)?.let { size ->
-                    drawScope.createBitmap(size = size, content = content).onSuccess { bmp ->
-                        setPixelMap(bmp.toPixelMap())
-                    }.getOrNull()
-                }
-            }
-        }
-
-        bitmap?.run {
-            Image(bitmap = this, contentDescription = null, modifier = Modifier.fillMaxSize().pointerInput(mapOffset) {
-                detectTapGestures(onTap = { offset ->
-                    indicatorOffsetPercent = (mapOffset?.invoke(this, offset) ?: offset).let { (x, y) ->
-                        Offset(
+        Canvas(modifier = Modifier.fillMaxSize().pointerInput(updatedMapIndicatorOffset) {
+            detectTapGestures(onTap = { offset ->
+                updatedOnIndicatorOffsetPercentage(
+                    (updatedMapIndicatorOffset?.invoke(this, offset) ?: offset).let { (x, y) ->
+                        OffsetPercentage(
                             x = (x / size.width).coerceIn(0f, 1f),
                             y = (y / size.height).coerceIn(0f, 1f),
                         )
                     }
-                })
-            }.pointerInput(mapOffset) {
-                detectDragGestures { change, _ ->
-                    change.consume()
-                    change.position.let { offset ->
-                        indicatorOffsetPercent = (mapOffset?.invoke(this, offset) ?: offset).let { (x, y) ->
-                            Offset(
+                )
+            })
+        }.pointerInput(updatedMapIndicatorOffset) {
+            detectDragGestures { change, _ ->
+                change.consume()
+                change.position.let { offset ->
+                    updatedOnIndicatorOffsetPercentage(
+                        (updatedMapIndicatorOffset?.invoke(this, offset) ?: offset).let { (x, y) ->
+                            OffsetPercentage(
                                 x = (x / size.width).coerceIn(0f, 1f),
                                 y = (y / size.height).coerceIn(0f, 1f),
                             )
                         }
-                    }
-                }
-            })
-
-            Canvas(modifier = Modifier.fillMaxSize()) {
-                if (indicatorOffset.isSpecified) {
-                    indicatorContent(indicatorOffset)
+                    )
                 }
             }
+        }) {
+            content()
+
+            indicatorContent(indicatorOffset)
         }
     }
 }
