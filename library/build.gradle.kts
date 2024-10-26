0ecb2380684bd28e6ffc523e83c74fc93c455c7d
numq <numq.dev@gmail.com>
2024-10-26 16:10:55 +0300

    Provide refactoring

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.0"
     id("org.jetbrains.compose") version "1.6.10"
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
+    id("maven-publish")
 }
 
 group = "com.github.numq"
@@ -26,4 +27,15 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = rootProject.name
+            version = project.version.toString()
+        }
+    }
 }
